ade70db86691747025739469dc47efb1a7e4d200
olive-hanmer-h010783p <h010783p@student.staffs.ac.uk>
2025-10-03 17:17:58 +0100

    update

--- big boat/big boat.cpp
+++ big boat/big boat.cpp
@@ -9,7 +9,7 @@ int main()
 	int x;
 	cout << "type length ";
 	cin >> x;
-	
+
 	int y;
 	cout << "type height ";
 	cin >> y;
@@ -24,7 +24,8 @@ int main()
 	string boat3 = " *       * \n";
 	string boat4 = "  *******  \n";
 
-	if (x >= 0 and y >= 0)
+	if (x <= 0 and y <= 0)
+	{
 		cout << flag1;
 		cout << flag2;
 		cout << flag3;
@@ -34,23 +35,25 @@ int main()
 		cout << boat2;
 		cout << boat3;
 		cout << boat4;
+	}
+		
 
 	if (x > 0)
 		while (x > 1)
 		{
-			flag1.insert(1," ");
-			flag2.insert(1," ");
-			flag3.insert(1," ");
-			flag4.insert(1," ");
-			pole.insert(1," ");
-			boat1.insert(1,"*");
-			boat2.insert(3," ");
-			boat3.insert(3," ");
-			boat4.insert(4,"*");
+			flag1.insert(1, " ");
+			flag2.insert(1, " ");
+			flag3.insert(1, " ");
+			flag4.insert(1, " ");
+			pole.insert(1, " ");
+			boat1.insert(1, "*");
+			boat2.insert(3, " ");
+			boat3.insert(3, " ");
+			boat4.insert(4, "*");
 			x = x - 1;
-			
+
 		}
-		
+
 	cout << flag1;
 	cout << flag2;
 	cout << flag3;
